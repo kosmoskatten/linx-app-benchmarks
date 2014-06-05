c63cc942edad5f9b1a069fbb6768a2418e44583d
Patrik Sandahl <patrik.sandahl@ericsson.com>
2014-06-05 17:44:56 +0200

    Some more structure on the ping pairs application.

--- src/PingPairs.hs
+++ src/PingPairs.hs
@@ -1,29 +1,49 @@
 module Main where
 
+import Control.Concurrent.Async
+import Control.Exception (bracket)
+import Network.Linx.Gateway
 import System.Environment (getArgs)
+
+type GWAddress = (String, Int)
+type DelaySpec = [(Int, Int)]
 
 -- | The PingPairs program is establishing N number of "ping pairs".
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [gateway, gatewayPort, pairs, frequencyTable] ->
-      runPingPairs gateway (read gatewayPort) 
-                           (read pairs) 
-                           (read frequencyTable)
+    [gateway, gatewayPort, pairs, delaySpec] ->
+      runPingPairs (gateway, (read gatewayPort))
+                   (read pairs) 
+                   (read delaySpec)
     _                                             -> help
       
 
-runPingPairs :: String -> Int -> Int -> [(Int, Int)] -> IO ()
-runPingPairs gw gwPort num freqs = do
-  let freqs' = expandFrequencies freqs
-  print gw
-  print gwPort
-  print num
-  print freqs'
+runPingPairs :: GWAddress -> Int -> DelaySpec -> IO ()
+runPingPairs gwAddress num delaySpec = do
+  let delays = expandDelaySpec delaySpec
+  r <- mapConcurrently (pingPair gwAddress delays) [1..num]
+  print r
 
-expandFrequencies :: [(Int, Int)] -> [Int]
-expandFrequencies = concat . map (uncurry replicate)
+pingPair :: GWAddress -> [Int] -> Int -> IO [Int]
+pingPair gwAddress delays myId =
+  withAsync (pingServer gwAddress myId) (pingClient gwAddress delays myId)
+  
+pingServer :: GWAddress -> Int -> IO ()
+pingServer gwAddress myId = return ()
+
+pingClient :: GWAddress -> [Int] -> Int -> Async () -> IO [Int]
+pingClient gwAddress delays myId _ = return [myId]
+
+serverId :: Int -> String
+serverId n = "server" ++ (show n)
+
+clientId :: Int -> String
+clientId n = "client" ++ (show n)
+           
+expandDelaySpec :: DelaySpec -> [Int]
+expandDelaySpec = concat . map (uncurry replicate)
 
 help :: IO ()
 help = do
