ea3c675a3c3f65864a61ed6ef03a9fdac4521e71
Patrik Sandahl <patrik.sandahl@ericsson.com>
2014-06-08 22:46:33 +0200

    Restructured the program into separate executables for client and server.

--- src/PingPairs.hs
+++ src/PingPairs.hs
@@ -1,97 +1,128 @@
-{-# LANGUAGE CPP, BangPatterns #-}
+{-# LANGUAGE CPP #-}
 module Main where
 
 import Control.Applicative ((<$>))
 import Control.Concurrent (threadDelay)
-import Control.Concurrent.Async
+import Control.Concurrent.Async (async, mapConcurrently, wait)
 import Control.Exception (bracket)
 import Control.Monad (forever)
 import Data.Binary
-import Data.Time (getCurrentTime)
-import Data.Time.Clock (NominalDiffTime, diffUTCTime)
+import Data.Time (getCurrentTime, diffUTCTime)
+import Data.Time.Clock (NominalDiffTime)
 import Network.Linx.Gateway
 import System.Environment (getArgs)
-import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.ByteString.Lazy as LBS
 
-type GWAddress = (String, String)
-type DelaySpec = [(Int, Int)]
+type DelaySpecList = [(Int, Int)]
 
 #define HuntSig (SigNo 1)
-#define PingSig (SigNo 100)
-#define PongSig (SigNo 101)
+#define SetupRequestSig (SigNo 10)
+#define SetupReplySig (SigNo 11)
+#define PingRequestSig (SigNo 12)
+#define PingReplySig (SigNo 13)
+#define ByeSig (SigNo 14)
 
--- | The PingPairs program is establishing N number of "ping pairs".
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [gateway, gatewayPort, pairs, delaySpec] ->
-      runPingPairs (gateway, gatewayPort)
-                   (read pairs) 
-                   (read delaySpec)
-    _                                        -> help
+    ["server", gateway, port]                -> 
+      runServer gateway port
+    ["client", gateway, port, pairs, delays] -> do
+      result <- runClient gateway port (read pairs) (read delays)
+      print result
+    _                                        -> do
+      putStrLn "Bla bla bla"
+      putStrLn "Bla bla bla"
       
-
-runPingPairs :: GWAddress -> Int -> DelaySpec -> IO ()
-runPingPairs gwAddress num delaySpec = do
-  let delays = expandDelaySpec delaySpec
-  r <- mapConcurrently (pingPair gwAddress delays) [1..num]
-  print r
-
-pingPair :: GWAddress -> [Int] -> Int -> IO [NominalDiffTime]
-pingPair gwAddress delays myId =
-  withAsync (pingServer gwAddress myId) (pingClient gwAddress delays myId)
-  
-pingServer :: GWAddress -> Int -> IO ()
-pingServer (ip, port) myId =
-  bracket (create ("server" # myId) ip (Service port))
+runServer :: HostName -> String -> IO ()
+runServer ip port = 
+  bracket (create "server" ip (Service port))
           destroy $ \gw ->
     forever $ do
-      (from, Signal PingSig lbs) <- receive gw $ Sel [PingSig]
-      sendWithSelf gw from $ Signal PongSig lbs    
+      (from, Signal SetupRequestSig lbs) <- receive gw $ Sel [SetupRequestSig]
+      let numServers = decodeSetupRequest lbs
+          names      = serviceNames numServers
+      servers <- mapM (async . pingServer ip port) names
+      sendWithSelf gw from $ Signal SetupReplySig (encodeSetupReply names)
+      mapM_ wait servers
 
-pingClient :: GWAddress -> [Int] -> Int -> Async () -> IO [NominalDiffTime]
-pingClient (ip, port) delays myId _ =
-  bracket (create ("client" # myId) ip (Service port))
-          destroy $ \gw -> do
-    go gw delays [] =<< connectServer gw ("server" # myId)
+serviceNames :: Int -> [String]
+serviceNames n = map ("server" #) [1..n]
+
+pingServer :: HostName -> String -> String -> IO ()
+pingServer ip port name =
+  bracket (create name ip (Service port)) destroy go
   where
-    go :: Gateway -> [Int] -> [NominalDiffTime] -> Pid -> IO [NominalDiffTime]
-    go _ [] result _ = return $ reverse result
-    go gw (t:ts) result pid = do
-      timestamp <- serializedTimestamp
-      sendWithSelf gw pid $ Signal PingSig timestamp
-      (_, Signal PongSig lbs) <- receive gw $ Sel [PongSig]
-      !rtt <- captureRTT lbs
-      if not (null ts) then do
-        threadDelay t
-        go gw ts (rtt:result) pid
-        else go gw [] (rtt:result) pid
-      
-    connectServer :: Gateway -> String -> IO Pid
-    connectServer gw service = do
-      _ <- hunt gw service $ NumericSignal HuntSig
-      (pid, NumericSignal HuntSig) <- receive gw $ Sel [HuntSig]
-      return pid
+    go :: Gateway -> IO ()
+    go gw = do
+      (from, signal) <- receive gw $ Sel [PingRequestSig, ByeSig]
+      case signal of
+        Signal PingRequestSig lbs -> do
+          sendWithSelf gw from $ Signal PingReplySig lbs
+          go gw
+        _                         -> return ()
 
-serializedTimestamp :: IO LBS.ByteString
-serializedTimestamp = encode . show <$>  getCurrentTime
+runClient :: HostName -> String -> Int -> DelaySpecList 
+          -> IO [[NominalDiffTime]]
+runClient ip port pairs delays = 
+  bracket (create "client" ip (Service port))
+          destroy $ \gw -> do
+    pid <- connectService gw "server"
+    sendWithSelf gw pid $ Signal SetupRequestSig (encodeSetupRequest pairs)
+    (_, Signal SetupReplySig lbs) <- receive gw $ Sel [SetupReplySig]
+    let servers = decodeSetupReply lbs
+        clients = map ("client" #) [1..pairs]
+        delays' = extractDelaySpecs delays
+    mapConcurrently (pingClient ip port delays') $ zip clients servers
+    
+pingClient :: HostName -> String -> [Int] -> (String, String) 
+           -> IO [NominalDiffTime]
+pingClient ip port delays (me, server) =
+  bracket (create me ip (Service port))
+          destroy $ \gw -> do
+    pid <- connectService gw server
+    go gw pid delays []
+  where
+    go :: Gateway -> Pid -> [Int] -> [NominalDiffTime] -> IO [NominalDiffTime]
+    go gw pid [] result = do
+      sendWithSelf gw pid $ NumericSignal ByeSig
+      return $ reverse result
+    go gw pid (t:ts) result = do
+      timestamp <- encodedTimestamp
+      sendWithSelf gw pid $ Signal PingRequestSig timestamp
+      (_, Signal PingReplySig lbs) <- receive gw $ Sel [PingReplySig]
+      rtt <- captureRtt lbs
+      threadDelay t
+      go gw pid ts (rtt:result)
 
-captureRTT :: LBS.ByteString -> IO NominalDiffTime
-captureRTT lbs = do
-  currentTime <- getCurrentTime
-  return $ diffUTCTime currentTime $ (read . decode) lbs
+connectService :: Gateway -> String -> IO Pid
+connectService gw service = do
+  _ <- hunt gw service $ NumericSignal HuntSig
+  fst <$> receive gw (Sel [HuntSig])
+  
+decodeSetupRequest :: LBS.ByteString -> Int
+decodeSetupRequest = decode
+
+encodeSetupRequest :: Int -> LBS.ByteString
+encodeSetupRequest = encode
+
+decodeSetupReply :: LBS.ByteString -> [String]
+decodeSetupReply = decode
+
+encodeSetupReply :: [String] -> LBS.ByteString
+encodeSetupReply = encode
+
+extractDelaySpecs :: DelaySpecList -> [Int]
+extractDelaySpecs = concat . map (uncurry replicate)
 
 (#) :: String -> Int -> String
-(#) name number = name ++ (show number)
+(#) service num = service ++ (show num)
 
-expandDelaySpec :: DelaySpec -> [Int]
-expandDelaySpec = concat . map (uncurry replicate)
+encodedTimestamp :: IO LBS.ByteString
+encodedTimestamp = encode . show <$> getCurrentTime
 
-help :: IO ()
-help = do
-  putStrLn "Usage: PingPairs <gateway address> <gateway port> <pairs> <freqs>"
-  putStrLn " <gateway address>: IP address to gateway. E.g. 192.168.122.8"
-  putStrLn " <gateway port>   : Port to gateway. E.g. 21768"
-  putStrLn " <pairs>          : Communicating pairs. E.g. 7"
-  putStrLn " <freqs>          : Ping intensity. E.g. \"[(10, 1000), (100, 100)]\""
+captureRtt :: LBS.ByteString -> IO NominalDiffTime
+captureRtt lbs = do
+  timestamp <- getCurrentTime
+  return $ timestamp `diffUTCTime` (read . decode) lbs
